3e8c6dc215b7ef7b7a5caefa3641b68cad1147d5
urasi <urasi@192.168.0.10>
2017-07-31 12:18:40 -0500

    fix path to log file

--- src/com/zixi/fserver/ScriptExecutor.java
+++ src/com/zixi/fserver/ScriptExecutor.java
@@ -79,7 +79,7 @@ public class ScriptExecutor {
 				MaxwellLogParser maxwellLogParser = new MaxwellLogParser();
 				
 				try {
-					return new Object [] {new Long(maxwellLogParser.getRunningTimeFromLogFile("root//log")), 
+					return new Object [] {new Long(maxwellLogParser.getRunningTimeFromLogFile("/root/log")), 
 					new Integer(maxwellLogParser.getNumberOfDroptsFromLogFile("/root/log"))};
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
